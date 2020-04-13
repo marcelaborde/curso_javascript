491a7af7cdd9a4351c6b606e46195c3053715d59
Marcela Borde Oliveira <marcelabordemb@gmail.com>
2020-04-13 14:57:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function clicou(){
+    alert("Obrigado por clicar");
+}
+
+
+/*function soma(n1,n2){
+    return n1+n2;
+}
+alert(soma(5,10));*/
+
+
+/*
+var d = new Date();
+alert(d); */
+
+
+
+/*var count;
+for(count=0; count <=5; cont++){
+    alert(count);
+}; */
+
+/*var count = 0;
+while (count <5){
+    console.log(count);
+    alert(count);
+    count++;
+}*/
+
+/*var idade = prompt("Qual sua idade?");
+if (idade >=18) {
+    alert ("Maior de idade")
+}
+else{
+alert ("Menor de idade")
+}; */
+
+/*var frutas = [{nome:"pera", cor:"amarela"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome); */
+
+/*var nome = "Marcela Borde Oliveira";
+var idade = 29;
+var idade2 = 33;
+var frase = "Japão é o melhor time do mundo"
+//alert("Bem vinda "+ nome + " tem " + idade + " anos")
+//alert (idade + idade2);
+console.log(nome);
+console.log(idade+idade2)
+console.log(frase.toLowerCase("Japão", "Brasil"));*/
